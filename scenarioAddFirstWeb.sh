6b5589cc0aff935e93f3b1ec1a9704b98e04155e
Andrii Krasovskyi <arnio.if@gmail.com>
2019-03-10 02:09:56 +0200

    Add mariadb config

---
+++ scenarioAddFirstWeb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+webIP=$1
+webName=$2
+#sudo sed -i "/    upstream moodle {/a\      server $WEBHOST;" /etc/nginx/nginx.conf
+cat <<EOF | sudo tee -a /home/vagrant/host.txt
+[first_web_server]
+$webName ansible_host=$webIP ansible_user=vagrant ansible_ssh_private_key_file=/home/vagrant/.ssh/$webIP.pem
+
+[web_server]
+$webName ansible_host=$webIP ansible_user=vagrant ansible_ssh_private_key_file=/home/vagrant/.ssh/$webIP.pem
+EOF
+sudo chmod 500 /home/vagrant/.ssh/*.pem
